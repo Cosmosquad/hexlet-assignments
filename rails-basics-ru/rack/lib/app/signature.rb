f3dd17f6b084782f5b40352a34f5223f8585f6ac
@hexlet/cli <support@hexlet.io>
2022-11-05 15:25:42 +0100

    submit rails-basics-ru/rack

--- rails-basics-ru/rack/lib/app/signature.rb
+++ rails-basics-ru/rack/lib/app/signature.rb
@@ -10,8 +10,8 @@ class Signature
   def call(env)
     # BEGIN
     status, headers, body = @app.call(env)
-    signature = Digest::SHA256.hexdigest(body[0])
-    [status, headers, "#{body}\n#{signature}"]
+    hash = Digest::SHA256.hexdigest body.first
+    [status, headers, body << hash]
     # END
   end
 end
