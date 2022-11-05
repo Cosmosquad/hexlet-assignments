5c4761fc82aa21118b8c6c0ca68e43ad70b49d9b
@hexlet/cli <support@hexlet.io>
2022-11-05 16:49:10 +0100

    submit rails-basics-ru/rack

--- rails-basics-ru/rack/lib/app/admin_policy.rb
+++ rails-basics-ru/rack/lib/app/admin_policy.rb
@@ -7,13 +7,7 @@ class AdminPolicy
 
   def call(env)
     # BEGIN
-    status, headers, body = @app.call(env)
-    request = Rack::Request.new(env)
-    if request.path.match?(%r{\A/admin})
-      [403, {}, []]
-    else
-      [status, headers, body]
-    end
+    
     # END
   end
 end
--- rails-basics-ru/rack/lib/app/router.rb
+++ rails-basics-ru/rack/lib/app/router.rb
@@ -5,13 +5,7 @@ require 'rack'
 class Router
   def call(env)
     # BEGIN
-    request = Rack::Request.new env
-    case request.path
-    when '/' then [200, { 'Content-Type' => 'text/plain' }, ['Hello, World!']]
-    when '/about' then [200, { 'Content-Type' => 'text/plain' }, ['About page']]
-    else
-      [404, { 'Content-Type' => 'text/plain' }, ['404 Not Found']]
-    end
+    
     # END
   end
 end
