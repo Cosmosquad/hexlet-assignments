f53d875e48913c71aee80ab0ab78d715ba0cf8ce
@hexlet/cli <support@hexlet.io>
2022-11-06 21:48:29 +0100

    submit rails-basics-ru/models

--- rails-basics-ru/models/config/routes.rb
+++ rails-basics-ru/models/config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get 'articles/show'
   root 'home#index'
   # BEGIN
-  
+  resources :articles, only: %i[index show]
   # END
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
